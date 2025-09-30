20dfb405708310fd3f19a36cfcb1618a4ec6366f
sarasaid180 <sara.mohammedali180@gmail.com>
2025-10-01 00:07:17 +0300

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -22,7 +22,7 @@ CREATE TABLE bronze.crm_cust_info(
 	cst_gndr            varchar(50),
 	cst_create_date     DATE
 );
-
+GO
 
 IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
     DROP TABLE bronze.crm_prd_info;
@@ -36,7 +36,7 @@ CREATE TABLE bronze.crm_prd_info(
 	prd_start_dt		 DATETIME,
 	prd_end_dt			 DATETIME
 );
-
+GO
 
 IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
     DROP TABLE bronze.crm_sales_details;
@@ -52,7 +52,7 @@ CREATE TABLE bronze.crm_sales_details (
     sls_quantity INT,
     sls_price    INT
 );
-
+GO
 
 IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
     DROP TABLE bronze.erp_loc_a101;
@@ -61,7 +61,7 @@ CREATE TABLE bronze.erp_loc_a101 (
     cid    NVARCHAR(50),
     cntry  NVARCHAR(50)
 );
-
+GO
 
 IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
     DROP TABLE bronze.erp_cust_az12;
@@ -71,7 +71,7 @@ GO
     bdate  DATE,
     gen    NVARCHAR(50)
 );
-
+GO
 
 IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
     DROP TABLE bronze.erp_px_cat_g1v2;
@@ -82,3 +82,4 @@ CREATE TABLE bronze.erp_px_cat_g1v2 (
     subcat       NVARCHAR(50),
     maintenance  NVARCHAR(50)
 );
+GO
