587b79e853cb29c24d37db1816c5f0447a201ee5
sarasaid180 <sara.mohammedali180@gmail.com>
2025-10-01 00:08:41 +0300

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -6,7 +6,8 @@ DDL Script: Create Bronze Tables ( Creating Tables Structure DDL)
 Script Purpose:
     This script creates tables in the 'bronze' schema, dropping existing tables 
     if they already exist.
-	  Run this script to re-define the DDL structure of 'bronze' Tables
+
+ * Running this script will re-define the DDL structure of 'bronze' Tables
 ===============================================================================
 */
 
